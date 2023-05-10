e4993a0e15d922e8791a9a4c4d5c9056ad52b3fd
Kirbi Huerta <kirbyxavier2@outlook.com>
2023-05-10 13:17:11 -0600

    si

--- src/ingenieria_caja_registradora/SistemaPuntodeVenta.java
+++ src/ingenieria_caja_registradora/SistemaPuntodeVenta.java
@@ -1,8 +1,11 @@
+
+import sistemapuntodeventa.SistemaBancario;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package sistemapuntodeventa;
+
 
 /**
  *
